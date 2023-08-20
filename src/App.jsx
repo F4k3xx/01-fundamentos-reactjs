d3e1474c41d4d6e37887a24700798e74cfe30adb
welly <wellyntonspagnol10@gmail.com>
2023-08-20 19:58:46 -0300

    Temp 2 Estrutura de aplicaçao: CSS modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Post from "./Post"
 function App() {
 
   return (
-    <Post
-    author="Diego Fernandes"
-    content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.Ad vero officia aspernatur est laudantium dolorum illum minima molestiae.Eos nulla tempore neque consectetur dolorum asperiores tenetur at non, dolores nemo!" />
+    <div>
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.Ad vero officia aspernatur est laudantium dolorum illum minima molestiae.Eos nulla tempore neque consectetur dolorum asperiores tenetur at non, dolores nemo!" />
+
+      <Post
+        author="Lucas Fernandes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.Ad vero officia aspernatur est laudantium dolorum illum minima molestiae.Eos nulla tempore neque consectetur dolorum asperiores tenetur at non, dolores nemo!" />
+    </div>
   )
 }
-  
+
 
 export default App
