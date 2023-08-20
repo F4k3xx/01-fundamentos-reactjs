f74afa501d936efe483004b053db63d4d41b6fb3
welly <wellyntonspagnol10@gmail.com>
2023-08-20 19:49:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./Post"
+
+function App() {
+
+  return (
+    <Post
+    author="Diego Fernandes"
+    content="Lorem ipsum dolor sit amet, consectetur adipisicing elit.Ad vero officia aspernatur est laudantium dolorum illum minima molestiae.Eos nulla tempore neque consectetur dolorum asperiores tenetur at non, dolores nemo!" />
+  )
+}
+  
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+function Post(props) {
+    console.log(props);
+
+    return (
+        <p>Post</p>
+    )
+}
+
+export default Post;
